a5e32f9830762f09759bb36785278b98ef7ad91e
Anthony Grear <tgrear2008@gmail.com>
2021-07-28 00:19:26 +0700

    Add sqlite3 database. Send email results in writing to the database

--- parent_emailer_V2/test.py
+++ parent_emailer_V2/test.py
@@ -2,20 +2,31 @@ import xlwings as xw
 import smtplib
 from email.message import EmailMessage
 from datetime import date
+from datetime import datetime
 import os
+import sqlite3
 office_user = os.environ.get('OFFICE_USER')
 office_pass = os.environ.get('OFFICE_PASS')
+old_value_path = 'C:\\Python\\python_work\\Personal Projects\\parent_emailer\\parent_emailer_V2\\old_value'
+
    
 def send_email():
     wb = xw.Book.caller()
     sheet = wb.sheets[2]
+    s_number = sheet.range("B1").value
+    s_name = sheet.range("B2").value
+    parent_email = (sheet.range("B3").value)
+    the_subject = (sheet.range("B6").value)    
+    the_content = sheet.range("B8").value
+    the_cc = sheet.range("B4").value    
+    date_now = datetime.now()
     
     msg = EmailMessage()
-    msg.set_content(sheet.range("B8").value)    
-    msg['Subject'] = (sheet.range("B6").value)
+    msg.set_content(the_content)    
+    msg['Subject'] = the_subject
     msg['From'] = office_user
-    msg['To'] = (sheet.range("B3").value)
-    msg['Cc'] = (sheet.range("B4").value)
+    msg['To'] = parent_email
+    msg['Cc'] = the_cc
     
     smtpsrv = 'smtp.office365.com'
     smtpserver = smtplib.SMTP(smtpsrv, 587)
@@ -24,7 +35,21 @@ def send_email():
     smtpserver.login(office_user, office_pass) 
     smtpserver.send_message(msg)
     smtpserver.close()
-    sheet.range("B2:B8").options(transpose = True).clear()
+    
+    file = open(old_value_path, 'r')
+    old_content = file.read()
+    
+    content_no_footer = the_content.replace(old_content, "")
+    
+    
+    conn = sqlite3.connect('C:\\Python\\python_work\\Personal Projects\\parent_emailer\\parent_emailer_V2\\parent_email_log.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO 'messages' VALUES (?, ?, ?, ?, ?, ?, ?)", (s_number, s_name, parent_email, the_subject, content_no_footer, the_cc, date_now))
+    conn.commit()
+    conn.close()
+    
+    
+    sheet.range("B1:B8").options(transpose = True).clear()
     
 def get_parent_email():
     wb = xw.Book.caller()
@@ -92,4 +117,4 @@ def get_carbon_copy():
 def clear_form():
     wb = xw.Book.caller()
     sheet = wb.sheets[2]
-    sheet.range("B2:B8").options(transpose = True).clear()
+    sheet.range("B1:B8").options(transpose = True).clear()
